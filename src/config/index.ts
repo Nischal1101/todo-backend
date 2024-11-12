48af2b737b0150fd5011f5c5c2cefac6eb5cf136
nischal1101 <kaflenk123@gmail.com>
2024-11-12 20:43:23 +0545

    add token secrets

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,17 @@
 import { config } from "dotenv";
 config();
 
-const { PORT, NODE_ENV, DATABASE_URL } = process.env;
+const {
+    PORT,
+    NODE_ENV,
+    DATABASE_URL,
+    JWT_ACCESS_SECRET,
+    ACCESS_TOKEN_EXPIRY_TIME,
+} = process.env;
 export const Config = {
     PORT,
     NODE_ENV,
     DATABASE_URL,
+    JWT_ACCESS_SECRET,
+    ACCESS_TOKEN_EXPIRY_TIME,
 };

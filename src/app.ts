5af0a378a115de2d28168f6e9f4fddbcb0857789
nischal1101 <kaflenk123@gmail.com>
2024-11-13 19:06:08 +0545

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import error from "./middlewares/error";
 import notFoundError from "./utils/notFoundError";
 import authRouter from "./routes/auth.routes";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({ credentials: true }));
+app.use(urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use("/api/auth", authRouter);
 app.use(error);

a03b1d56515a0fb4034bbc55a07755d28a0bdda8
nischal1101 <kaflenk123@gmail.com>
2024-11-13 21:30:46 +0545

    add admin privilage

---
+++ src/middlewares/admin.ts
@@ -0,0 +1,7 @@
+import { NextFunction, Request, Response } from "express";
+import CustomErrorHandler from "../utils/CustomErrorHandler";
+
+export const admin = (req: Request, res: Response, next: NextFunction) => {
+    if (req.user.role != admin) next();
+    else return next(new CustomErrorHandler(401, "unauthorized"));
+};

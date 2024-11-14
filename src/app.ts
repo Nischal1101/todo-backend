54fc922ac27a8bb8e992617c1b02d5547e7b3f50
nischal1101 <kaflenk123@gmail.com>
2024-11-14 09:21:42 +0545

    add helmet package for protection

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,27 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import express, { urlencoded } from "express";
 import error from "./middlewares/error";
 import notFoundError from "./utils/notFoundError";
 import authRouter from "./routes/auth.routes";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import { JwtPayload } from "jsonwebtoken";
 
 const app = express();
 
+app.use(cookieParser());
+declare global {
+    namespace Express {
+        interface Request {
+            user: JwtPayload;
+            cookies: { accesstoken?: string };
+        }
+    }
+}
 app.use(cors({ credentials: true }));
+app.use(helmet());
 app.use(urlencoded({ extended: false }));
 app.use(express.json());
 

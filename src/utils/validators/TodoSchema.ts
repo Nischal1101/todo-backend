e2d21e44df9286d67b1a9fe6a072e5586a4c4219
nischal1101 <kaflenk123@gmail.com>
2024-11-15 12:11:27 +0545

    finish todo schema

--- src/utils/validators/TodoSchema.ts
+++ src/utils/validators/TodoSchema.ts
@@ -2,18 +2,25 @@ import z from "zod";
 const priorityValues = ["high", "medium", "low"] as const;
 
 export const TodoSchema = z.object({
-    name: z.string({ message: "product name should be a string" }).min(4),
     description: z
-        .string({ message: "product description should be string" })
-        .min(8),
-    title: z.string({ message: "product price should be a string" }).min(10),
-    file: z.instanceof(File, {
-        message: "product image should be an image",
-    }),
+        .string({ required_error: " description is  a required field" })
+        .min(8, { message: "description must contain at least 8 chars" }),
+    title: z
+        .string({ required_error: "title is  a required field" })
+        .min(5, { message: "title must contain at least 5 chars" }),
+    // file: z.instanceof(File, {
+    //     message: "File should be an image.",
+    // }),
     priority: z.enum(priorityValues).default("medium"),
-    dueDate: z.date({
-        required_error: "Due date is required.",
-    }),
+    dueDate: z.string().refine(
+        (date) => {
+            const parsedDate = new Date(date);
+            return !isNaN(parsedDate.getTime()) && parsedDate > new Date();
+        },
+        {
+            message: "Due date must be a valid future date",
+        },
+    ),
 });
 
 export type todoSchema = z.infer<typeof TodoSchema>;

61bf2626e0e9907707ca39beb173eb1a143b4b77
nischal1101 <kaflenk123@gmail.com>
2024-11-15 12:09:38 +0545

    bug fix in admin middleware

--- src/middlewares/admin.ts
+++ src/middlewares/admin.ts
@@ -2,6 +2,6 @@ import { NextFunction, Request, Response } from "express";
 import CustomErrorHandler from "../utils/CustomErrorHandler";
 
 export const admin = (req: Request, res: Response, next: NextFunction) => {
-    if (req.user.role != admin) next();
-    else return next(new CustomErrorHandler(401, "unauthorized"));
+    if (req.user.role === admin) next();
+    else return next(new CustomErrorHandler(403, "unauthorized"));
 };
